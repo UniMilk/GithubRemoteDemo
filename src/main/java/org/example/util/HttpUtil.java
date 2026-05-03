42fa2d475435037c34fcd2e19b1f4514afc69302
unimilk <unimilk@example.com>
2026-05-03 18:45:53 +0800

    add(util): add a get method in HttpUtil.java

---
+++ src/main/java/org/example/util/HttpUtil.java
@@ -0,0 +1,20 @@
+package org.example.util;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpUtil {
+    public String get(String url) {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpGet getReq = new HttpGet(url);
+            CloseableHttpResponse resp = client.execute(getReq);
+            return EntityUtils.toString(resp.getEntity());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
